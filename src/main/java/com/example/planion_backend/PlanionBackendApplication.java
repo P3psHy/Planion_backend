6ad16465374b187ceba0a12cf37c3dce7eeba730
P3psHy <76128319+P3psHy@users.noreply.github.com>
2026-08-11 21:07:01 +0200

    feat: base

---
+++ src/main/java/com/example/planion_backend/PlanionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.planion_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/planion_backend/PlanionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planion_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
